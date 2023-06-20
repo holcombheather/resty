fdbab5e0f187707737488098d43844eade5cd361
Heather Holcomb <holcombheather@gmail.com>
2023-06-20 16:33:10 -0700

    update components from class to functions, imported and created scss

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+import './Footer.scss';
+
+function Footer() {
+  return <footer>&copy; 2018</footer>;
 }
 
 export default Footer;
